0d372eb8c485fc1802dde50fe1ced3ea4716cf2d
doubleknd26 <doubleknd26@gmail.com>
2018-12-11 23:02:19 +0900

    blueprint

---
+++ indexer/src/main/java/com/doubleknd26/moving/indexer/utils/ScalaUtils.java
@@ -0,0 +1,13 @@
+package com.doubleknd26.moving.indexer.utils;
+
+import scala.reflect.ClassTag;
+
+/**
+ * Created by doubleknd26 on 2018-12-11.
+ */
+public class ScalaUtils {
+
+    public static ClassTag getClassTag(Class clazz) {
+        return scala.reflect.ClassTag$.MODULE$.apply(clazz);
+    }
+}
---
+++ indexer/src/test/java/com/doubleknd26/moving/indexer/transform/IndexTransformerTest.java
@@ -0,0 +1,10 @@
+package com.doubleknd26.moving.indexer.transform;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by coupang on 2018-12-11.
+ */
+public class IndexTransformerTest {
+
+}
